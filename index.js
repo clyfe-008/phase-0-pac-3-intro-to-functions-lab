58284987d26983a9508a23941d76610ff6a28aa3
clyfe <cliff.kamau@student.moringaschool.com>
2023-06-09 14:51:49 +0300

    completed assignment

---
+++ index.js
@@ -0,0 +1,6 @@
+function shout(string) {
+    return string.touppercase();
+}
+function whisper(string){
+    return string.tolowercase();
+}
---
+++ test/index-test.js
@@ -0,0 +1,8 @@
+describe("shout(string)", function () {
+    it("receives one argument and returns it in all caps", function () {
+      // we'll get to this in a sec
+    });
+  });
+  describe("whisper(string)"),function (){
+    it("receives one argument and return it in lower case");
+  }
